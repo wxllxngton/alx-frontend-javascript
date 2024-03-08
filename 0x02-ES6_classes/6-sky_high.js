e5160312a3e27f4d8bbff1b4e081b91b93a6b789
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2024-03-08 15:05:16 +0300

    Added: Task 6 and 7

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -2,52 +2,37 @@
  * Represents the SkyHighBuilding class, extending the Building class.
  * @class
  */
-import Building from './5-building';
+import Building from './5-building.js';
 
-class SkyHighBuilding extends Building {
-  /**
+export default class SkyHighBuilding extends Building {
+    /**
      * Constructs a SkyHighBuilding object.
      *
-     * @param {number} sqft - The square footage of the skyHighBuilding.
-     * @param {number} floors - The floors of the skyHighBuilding.
+     * @param {number} sqft - The square footage of the SkyHighBuilding.
+     * @param {number} floors - The floors of the SkyHighBuilding.
      * @throws {TypeError} If the provided sqft is not a number.
      */
-  constructor(sqft, floors) {
-    super(sqft);
-    this.floors = floors;
-  }
-
-  /**
-     * Sets the floors of the SkyHighBuilding.
-     *
-     * @param {number} floors - The floors.
-     * @throws {TypeError} If the provided floors is not a number.
-     */
-  set floors(floors) {
-    if (typeof floors !== 'number') {
-      throw new TypeError('Floors must be a number');
+    constructor(sqft, floors) {
+        super(sqft);
+        this._floors = floors;
     }
-    this._floors = floors;
-  }
 
-  /**
+    /**
      * Gets the floors of the SkyHighBuilding.
      *
      * @returns {number} The floors.
      */
-  get floors() {
-    return this._floors;
-  }
+    get floors() {
+        return this._floors;
+    }
 
-  /**
-     * Provides an evacuation warning message for the SkyHighBuilding.
+    /**
+     * Overrides the evacuationWarningMessage method to provide a custom message.
      *
      * @override
      * @returns {string} The evacuation warning message.
      */
-  evacuationWarningMessage() {
-    return `Evacuate slowly from the ${this._floors} floors of the SkyHighBuilding`;
-  }
+    evacuationWarningMessage() {
+        return `Evacuate slowly the ${this._floors} floors`;
+    }
 }
-
-export default SkyHighBuilding;
