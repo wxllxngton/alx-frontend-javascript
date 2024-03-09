79c6047eaa31b8aa71ace1f772a249de5249e2ce
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2024-03-09 05:50:03 +0300

    :broom: eslint reformatting

--- 0x02-ES6_classes/6-sky_high.js
+++ 0x02-ES6_classes/6-sky_high.js
@@ -5,34 +5,34 @@
 import Building from './5-building.js';
 
 export default class SkyHighBuilding extends Building {
-    /**
+  /**
      * Constructs a SkyHighBuilding object.
      *
      * @param {number} sqft - The square footage of the SkyHighBuilding.
      * @param {number} floors - The floors of the SkyHighBuilding.
      * @throws {TypeError} If the provided sqft is not a number.
      */
-    constructor(sqft, floors) {
-        super(sqft);
-        this._floors = floors;
-    }
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
 
-    /**
+  /**
      * Gets the floors of the SkyHighBuilding.
      *
      * @returns {number} The floors.
      */
-    get floors() {
-        return this._floors;
-    }
+  get floors() {
+    return this._floors;
+  }
 
-    /**
+  /**
      * Overrides the evacuationWarningMessage method to provide a custom message.
      *
      * @override
      * @returns {string} The evacuation warning message.
      */
-    evacuationWarningMessage() {
-        return `Evacuate slowly the ${this._floors} floors`;
-    }
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this._floors} floors`;
+  }
 }
--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -3,59 +3,59 @@
  * @class
  */
 export default class Airport {
-    /**
+  /**
      * Constructs an Airport object.
      *
      * @param {string} name - The name of the airport.
      * @param {string} code - The code of the airport.
      */
-    constructor(name, code) {
-        this.name = name;
-        this.code = code;
-    }
+  constructor(name, code) {
+    this.name = name;
+    this.code = code;
+  }
 
-    /**
+  /**
      * Gets the name of the airport.
      *
      * @returns {string} The name.
      */
-    get name() {
-        return this._name;
-    }
+  get name() {
+    return this._name;
+  }
 
-    /**
+  /**
      * Sets the name of the airport.
      *
      * @param {string} value - The name to set.
      */
-    set name(value) {
-        this._name = value;
-    }
+  set name(value) {
+    this._name = value;
+  }
 
-    /**
+  /**
      * Gets the code of the airport.
      *
      * @returns {string} The code.
      */
-    get code() {
-        return this._code;
-    }
+  get code() {
+    return this._code;
+  }
 
-    /**
+  /**
      * Sets the code of the airport.
      *
      * @param {string} value - The code to set.
      */
-    set code(value) {
-        this._code = value;
-    }
+  set code(value) {
+    this._code = value;
+  }
 
-    /**
+  /**
      * Gets the string tag for the airport.
      *
      * @returns {string} The code as the string tag.
      */
-    get [Symbol.toStringTag]() {
-        return this._code;
-    }
+  get [Symbol.toStringTag]() {
+    return this._code;
+  }
 }
