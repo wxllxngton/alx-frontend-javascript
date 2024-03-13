e8ef27ce131a9967d2c59dd9c8f09646392730ba
wxllxngton <jjngugi27@gmail.com>
2024-03-13 13:30:25 +0300

    :broom: reformatting code with eslint

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -6,13 +6,13 @@
  * @returns {string} A string containing all the set values that start with the specified string.
  */
 const cleanSet = (set, startString) => {
-    if (startString === undefined || startString.length === 0) {
-        return '';
-      }
-      return [...set]
-        .filter((str) => (str !== undefined ? str.startsWith(startString) : ''))
-        .map((str) => (str !== undefined ? str.slice(startString.length) : ''))
-        .join('-');
+  if (startString === undefined || startString.length === 0) {
+    return '';
+  }
+  return [...set]
+    .filter((str) => (str !== undefined ? str.startsWith(startString) : ''))
+    .map((str) => (str !== undefined ? str.slice(startString.length) : ''))
+    .join('-');
 };
 
 export default cleanSet;
