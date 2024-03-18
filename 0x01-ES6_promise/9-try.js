6e04cf74915c3f3928ebe802cc2c381c7eca586f
John Ombuya <79745456+wxllxngton@users.noreply.github.com>
2024-03-18 11:43:46 +0300

    :broom: eslint reformatting

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,10 +1,17 @@
+/**
+ * Creates and returns an array to track the execution of a math function.
+ *
+ * @param {Function} mathFunction - The math function to execute.
+ * @returns {Array} An array containing the results of the math function execution and
+ * error messages, if any, along with a message indicating that the guardrail was processed.
+ */
 export default function guardrail(mathFunction) {
   const queue = [];
 
   try {
     queue.push(mathFunction());
   } catch (error) {
-    queue.push('Error: ', String(error.message));
+    queue.push(`Error: ${String(error.message)}`);
   } finally {
     queue.push('Guardrail was processed');
   }
